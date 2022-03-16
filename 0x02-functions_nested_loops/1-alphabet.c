4c239bdb63d3369ce499a0748fcdf5cbf7bda408
Rhomeinel <rhomeinel@gmail.com>
2022-03-15 18:37:12 -0700

    modify decription-Betty doc style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code
- print_alphabet - prints the alphabet, in lowercase, followed by a new line*
- * Return: Always 0.
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  */
 
 void print_alphabet(void)
