daf3e44edc180e147a8caf2c3a69f83efcd3f813
Rhomeinel <rhomeinel@gmail.com>
2022-03-16 04:55:44 -0700

    modify task 6 code

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _abs - computes the absolute value of an integer
  * @n: the int to check
  * Return: the absolute value of int
  */
-
 int _abs(int n)
 {
 	if (n >= 0)
