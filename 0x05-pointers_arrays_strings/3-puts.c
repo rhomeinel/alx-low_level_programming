ccd7c321614cff3aa935217788d2d4432684ae9e
Rhomeinel <rhomeinel@gmail.com>
2022-03-21 16:42:25 -0700

    add code to task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - print a string
+ * @str: string
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 1;
+	}
+	_putchar('\n');
+}		
