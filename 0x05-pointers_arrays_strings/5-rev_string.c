0517ee1c14a8c29d5796fe4c8cb6bbbd71db8cf8
Rhomeinel <rhomeinel@gmail.com>
2022-03-21 17:03:57 -0700

    add code to task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - print a string in reverse followed by a new line
+ * @s: string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int len, count, end;
+	char beg;
+
+	len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	len--;
+	for (count = 0; count <= len / 2; count++)
+	{
+		beg = s[count];
+		end = s[len - count];
+		s[count] = end;
+		s[len - count] = beg;
+	}
+}
