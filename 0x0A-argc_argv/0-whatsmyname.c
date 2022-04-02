bb458b1f3d255f56b0ea65accfb1df6c15ea2cee
Rhomeinel <rhomeinel@gmail.com>
2022-04-01 21:04:52 -0700

    Modify code to task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- * main -  writes its name
- * @argc: arguement count
- * @argv: array of pointers to arguement strings
- * Return: 0
- **/
+* main - prints name of file
+* @argc: number of arguments
+* @argv: arguments entered
+* Description: print name of file
+* Return: zero
+**/
 int main(int argc, char *argv[])
 {
-	argc = argc;
-	printf("%s\n", *argv);
-	return (0);
+printf("%s\n", argv[0]);
+argc = argc;
+return (0);
 }
