f23b0e452d41e680770c5e8fc061e9c76231ab60
Rhomeinel <rhomeinel@gmail.com>
2022-04-01 20:55:09 -0700

    Modify code to task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-#include "holberton.h"
-
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
- */
-int main(int argc __attribute__((unused)), char *argv[])
+ * main -  writes its name
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
 {
+	argc = argc;
 	printf("%s\n", *argv);
-
 	return (0);
 }
