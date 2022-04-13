9058b855e7abbb26b6b8dad2b4e1e7b3c4df0305
Rhomeinel <rhomeinel@gmail.com>
2022-04-13 15:10:58 -0700

    Modify function prototype and data stuctures

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,10 @@
-#ifndef OPERATIONS
-#define OPERATIONS
+#ifndef CALC_H
+#define CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct op - Struct op
  *
@@ -10,11 +15,13 @@ typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-}	op_t;
+} op_t;
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
 #endif
